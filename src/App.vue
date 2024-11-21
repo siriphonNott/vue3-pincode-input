6a87d02e77c9b7f51bfffe026e0c8ff361d039fe
nottdark <nottdark.dev@gmail.com>
2024-11-22 00:28:02 +0700

    Adjust Preview secure

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@ const pincode = reactive({
       <VuePincodeInput v-model="pincode.default" :digits="4" autofocus />
       <div class="preview">pincode: {{ pincode.default }}</div>
       <div class="code-preview">
-        {{ `<VuePincodeInput v-model="pincode" />` }}
+        {{ `
+        <VuePincodeInput v-model="pincode" />` }}
       </div>
     </div>
     <div class="custom shadow">
@@ -31,7 +32,8 @@ const pincode = reactive({
       />
       <div class="preview">pincode: {{ pincode.custom }}</div>
       <div class="code-preview">
-        {{ `<VuePincodeInput
+        {{ `
+        <VuePincodeInput
           v-model="pincode"
           input-class="rounded-full w-18 h-18 text-3xl text-gray-500 border-2 border-gray-200 shadow"
           success-class="border-2 border-green-400"
@@ -39,19 +41,20 @@ const pincode = reactive({
           autofocus
         />` }}
       </div>
-      <div class="default shadow">
-        <h2>Preview secure</h2>
-        <hr />
-        <VuePincodeInput
-          v-model="pincode.default"
-          :digits="4"
-          :preview="200"
-          secure
-        />
-        <div class="preview">pincode: {{ pincode.default }}</div>
-        <div class="code-preview">
-          {{ `<VuePincodeInput v-model="pincode" :preview="200" secure />` }}
-        </div>
+    </div>
+    <div class="default shadow">
+      <h2>Preview secure</h2>
+      <hr />
+      <VuePincodeInput
+        v-model="pincode.default"
+        :digits="4"
+        :preview="200"
+        secure
+      />
+      <div class="preview">pincode: {{ pincode.default }}</div>
+      <div class="code-preview">
+        {{ `
+        <VuePincodeInput v-model="pincode" :preview="200" secure />` }}
       </div>
     </div>
   </main>
@@ -70,6 +73,7 @@ const pincode = reactive({
   align-items: center;
   gap: 2rem;
 }
+
 @media screen and (max-width: 480px) {
   .main-wrapper {
     padding: 15px;
@@ -87,6 +91,7 @@ const pincode = reactive({
   border: 1px gray dotted;
   border-radius: 0.5rem;
 }
+
 .main-wrapper hr {
   width: 100%;
   border-bottom: none;
@@ -100,6 +105,7 @@ const pincode = reactive({
   padding: 0.5rem 1rem;
   border-radius: 5px;
 }
+
 .code-preview {
   background: #1b1d22;
   border-radius: 5px;
@@ -109,6 +115,7 @@ const pincode = reactive({
   width: 100%;
   white-space: pre-wrap;
 }
+
 footer {
   padding: 1rem;
   width: 100%;
